f16baf6ead44fe362205235da9fb5ab4d6649c85
britainjs <britainjs@gmail.com>
2015-01-07 02:24:50 -0800

    Added style and got the dice tracker mostly working.

--- ergoNomic.js
+++ ergoNomic.js
@@ -3,27 +3,36 @@ DiceRolls = new Mongo.Collection('diceRolls');
 if (Meteor.isClient) {
   Template.body.events ({
     "click .roll-dice": function (event) {
-    // This function is called when the new task form is submitted
+      // This function is called when the new task form is submitted
 
-    var result = Math.floor(Math.random() * 6) + 1;
+      var result = Math.floor(Math.random() * 6) + 1;
+      var player = Meteor.user().profile ? Meteor.user().profile.name : Meteor.user().username;
 
-    DiceRolls.insert({
-      result: result,
-      createdAt: new Date(), // current time
-      player: Meteor.user()
-    });
+      DiceRolls.insert({
+        result: result,
+        createdAt: new Date(), // current time
+        player: player
+      });
 
-    alert(result)
-    return false  
+      Session.set("result", result)
+      return false
+    }  
   });
 
   Template.body.helpers ({
-
+    previousRolls: function () {
+      return DiceRolls.find({}, {limit: 6, sort: {createdAt: -1}  });
+    },
+    result: function () {
+      result = Session.get("result")
+      
+      result ? result : null;
+    }
   });
 
   Accounts.ui.config({
-  passwordSignupFields: "USERNAME_ONLY"
-});
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 }
 
 if (Meteor.isServer) {
